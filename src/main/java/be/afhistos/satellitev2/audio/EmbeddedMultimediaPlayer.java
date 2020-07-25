027bea5c5d87c75f9576083b58a27d288a28fecd
afhistos <afhistos@gmail.com>
2020-07-25 18:12:19 +0200

    Adding EmbeddedMultimediaPlayer ---WIP!---

--- src/main/java/be/afhistos/satellitev2/audio/EmbeddedMultimediaPlayer.java
+++ src/main/java/be/afhistos/satellitev2/audio/EmbeddedMultimediaPlayer.java
@@ -1,8 +1,11 @@
 package be.afhistos.satellitev2.audio;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEvent;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventListener;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 public class EmbeddedMultimediaPlayer extends AudioEventAdapter {
 
@@ -10,4 +13,8 @@ public class EmbeddedMultimediaPlayer extends AudioEventAdapter {
 
     }
 
+    @Override
+    public void onTrackStart(AudioPlayer player, AudioTrack track) {
+
+    }
 }
