95d0415d7fd3ac117ec3b1198885371a067cd4a3
afhistos <afhistos@gmail.com>
2020-07-31 23:15:32 +0200

    Fixing EMPDefaultEmbed

--- src/main/java/be/afhistos/satellitev2/audio/emp/EMPDefaultEmbed.java
+++ src/main/java/be/afhistos/satellitev2/audio/emp/EMPDefaultEmbed.java
@@ -11,14 +11,13 @@ import net.dv8tion.jda.api.entities.TextChannel;
 
 public class EMPDefaultEmbed extends EmbedBuilder {
     public EMPDefaultEmbed(AudioPlayer player, AudioTrack t, GuildMusicManager musicManager, TextChannel channel){
-        EmbedBuilder builder =  new DefaultEmbed(Satellite.getBot().getSelfUser());
         AudioTrackInfo info = t.getInfo();
-        builder.setThumbnail("https://img.youtube.com/vi/"+t.getIdentifier()+"/maxresdefault.jpg").setTitle(info.title, info.uri);
-        builder.setDescription("Chargement...");
-        builder.addField("Par "+info.author, "Volume: "+player.getVolume()+ "%      Basses: "
+        this.setThumbnail("https://img.youtube.com/vi/"+t.getIdentifier()+"/maxresdefault.jpg").setTitle(info.title, info.uri);
+        this.setDescription("Chargement...");
+        this.addField("Par "+info.author, "Volume: "+player.getVolume()+ "%      Basses: "
                 +(musicManager.isBassActive() ? "":"dés")+
                 "activées", false);
-        builder.addField("Contrôles:",":arrow_backward::arrow_forward: : Passer à la musique précédente/suivante\n" +
+        this.addField("Contrôles:",":arrow_backward::arrow_forward: : Passer à la musique précédente/suivante\n" +
                 ":arrow_up_small::arrow_down_small: : Gérer le volume   " +
                 ":twisted_rightwards_arrows: : Mélanger la liste de lecture\n" +
                 ":fast_forward: : Avancer de 10 secondes" +
@@ -27,12 +26,12 @@ public class EMPDefaultEmbed extends EmbedBuilder {
                 ":arrows_counterclockwise: : Répéter la musique en cours\n" +
                 ":wastebasket: : Supprimer le lecteur "+
                 ":loud_sound: : (Dés)activer les basses",false);
-        builder.addField("Status:", (player.isPaused() ? "En pause" : "En cours de lecture"), true);
+        this.addField("Status:", (player.isPaused() ? "En pause" : "En cours de lecture"), true);
         if(channel.getGuild().getAudioManager().isConnected()){
             int n = channel.getGuild().getAudioManager().getConnectedChannel().getMembers().size() - 1;
-            builder.addField("Auditeurs:", "Il y à **"+n+"** auditeur(s)", true);
+            this.addField("Auditeurs:", "Il y à **"+n+"** auditeur(s)", true);
         }
-        builder.addField("Liste de lecture:", "[ **"+musicManager.scheduler.getQueue().size()+
+        this.addField("Liste de lecture:", "[ **"+musicManager.scheduler.getQueue().size()+
                 "** musiques restantes]", true);
     }
 }
