b4ec0b5519fe8cf58a982f340fe8bd6879ac9efd
afhistos-git <32820096+afhistos@users.noreply.github.com>
2022-04-22 11:42:02 +0200

    Auth tokens fully supported

--- src/main/java/be/afhistos/satellitev2/commands/CommandVulcain.java
+++ src/main/java/be/afhistos/satellitev2/commands/CommandVulcain.java
@@ -36,25 +36,7 @@ public class CommandVulcain extends Command {
         boolean alreadyLinked = qr.getRowsCount() > 0;
         qr.close();
         if(event.getArgs().isEmpty()){
-            if(alreadyLinked){
-                event.replyInDm("**Récapitulatif**\n"+
-                        "Votre compte est déjà lié à Vulcain.\n"+
-                        "»Vous souhaitez générer un nouveau token ? `²vulcain new` (l'ancien sera désactivé)\n" +
-                        "»Vous souhaitez re-voir votre token ? `²vulcain show`\n" +
-                        "»Vous souhaitez supprimer votre token ? `²vulcain delete`\n" +
-                        "**COMING SOON**: Possibilité d'ajouter des collaborateurs.\n" +
-                        "__**RAPPEL: NE PARTAGEZ JAMAIS VOTRE TOKEN A QUI QUE CE SOIT! Si vous pensez que quelqu'un vous l'a dérobé," +
-                        "`²vulcain delete`.**__");
-            }else{
-                event.replyInDm("**Récapitulatif**\n" +
-                        "Votre compte n'est pas lié à Vulcain.\n" +
-                        "Vulcain est un logiciel qui permet de contrôler Ganymède, ainsi que modérer votre serveur discord" +
-                        " à distance, sur votre smartphone/tablette/ordinateur. Une présentation est disponible ici: " +
-                        "http://164.68.101.190/Vulcain\n" +
-                        "»Convaincu ? `²vulcain new` pour commencer !\n" +
-                        "»Vous n'êtes pas emballé ? Ce n'est pas grave! Ganymède fonctionne très bien sans lui, et seul le propriétaire d'un serveur" +
-                        " peut activer et utiliser Vulcain.");
-            }
+            event.replyInDm(getHelpMessage(alreadyLinked));
         }
         switch (event.getArgs()){
             case "new":
@@ -84,7 +66,42 @@ public class CommandVulcain extends Command {
                             .flatMap(Message::delete)
                             .queue();
                 }
+                break;
             }
+            case "delete":{
+                if(!alreadyLinked){
+                    event.replyInDm("Vous n'avez pas encore de token actif! `²vulcain new`");
+                }else{
+                    utils.execute("DELETE FROM auth WHERE user_id = ?", event.getAuthor().getId());
+                    event.replyInDm("Votre token à été supprimé.");
+                }
+                break;
+            }
+            default:{
+                event.replyInDm(getHelpMessage(alreadyLinked));
+            }
+        }
+    }
+
+    private String getHelpMessage(boolean alreadyLinked) {
+        if(alreadyLinked){
+            return "**Récapitulatif**\n"+
+                    "Votre compte est déjà lié à Vulcain.\n"+
+                    "»Vous souhaitez générer un nouveau token ? `²vulcain new` (l'ancien sera désactivé)\n" +
+                    "»Vous souhaitez re-voir votre token ? `²vulcain show`\n" +
+                    "»Vous souhaitez supprimer votre token ? `²vulcain delete`\n" +
+                    "**COMING SOON**: Possibilité d'ajouter des collaborateurs.\n" +
+                    "__**RAPPEL: NE PARTAGEZ JAMAIS VOTRE TOKEN A QUI QUE CE SOIT! Si vous pensez que quelqu'un vous l'a dérobé," +
+                    "`²vulcain delete`.**__";
+        }else{
+            return "**Récapitulatif**\n" +
+                    "Votre compte n'est pas lié à Vulcain.\n" +
+                    "Vulcain est un logiciel qui permet de contrôler Ganymède, ainsi que modérer votre serveur discord" +
+                    " à distance, sur votre smartphone/tablette/ordinateur. Une présentation est disponible ici: " +
+                    "http://164.68.101.190/Vulcain\n" +
+                    "»Convaincu ? `²vulcain new` pour commencer !\n" +
+                    "»Vous n'êtes pas emballé ? Ce n'est pas grave! Ganymède fonctionne très bien sans lui, et seul le propriétaire d'un serveur" +
+                    " peut activer et utiliser Vulcain.";
         }
     }
 
